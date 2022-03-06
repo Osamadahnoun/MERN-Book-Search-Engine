464b1ccd8d9927f599566e6f5ab5b990ae4d137f
Osama Dahnoun <omdahnou@ncsu.edu>
2022-03-06 01:10:13 -0500

    set up rough typedefs and resolvers / auth.js + server.js

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,44 @@
+// import the gql tagged template function
+const { gql } = require("apollo-server-express");
+
+// create our typeDefs
+const typeDefs = gql`
+  type Book {
+    bookId: Int
+    authors: [String]
+    description: String
+    title: String
+    image: String
+    link: String
+  }
+  type User {
+    _id: ID
+    username: String
+    email: String
+    bookCount: Int
+    savedBooks: [Book]
+  }
+  input BookInput {
+    authors: [String!]
+    description: String!
+    bookId: Int
+    image: String!
+    link: String!
+  }
+  type Query {
+    me: User
+  }
+  type Mutation {
+    login(email: String!, password: String!): Auth
+    addUser(username: String!, email: String!, password: String!): Auth
+    saveBook(input: BookInput): User
+    removeBook(bookId: Int!): User
+  }
+  type Auth {
+    token: ID!
+    user: User
+  }
+`;
+
+// export the typeDefs
+module.exports = typeDefs;
